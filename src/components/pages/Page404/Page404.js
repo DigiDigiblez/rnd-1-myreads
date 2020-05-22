5ad5aeca7baad8900aaabf4db4e60f231dbd73bb
Carl Bowen <carl.bowen@vibrantmedia.com>
2020-05-22 09:09:39 +0100

    feature: 404 page

--- src/components/pages/Page404/Page404.js
+++ src/components/pages/Page404/Page404.js
@@ -2,15 +2,26 @@ import "./Page404.css";
 
 import React from "react";
 
+import { NavLink } from "react-router-dom";
 import Container from "../../atoms/Container";
 import Chrome from "../../templates/Chrome";
+import { ROUTES } from "../Routes/route";
 
 const Page404 = () => {
   const baseclass = "page404";
 
   return (
     <Container className={baseclass}>
-      <Chrome>404 page not found!</Chrome>
+      <Chrome>
+        <section className={`${baseclass}__contents`}>
+          404 page not found!
+          <NavLink to={ROUTES.LIST}>
+            <button className={`${baseclass}__button`}>
+              Let&apos;s go home
+            </button>
+          </NavLink>
+        </section>
+      </Chrome>
     </Container>
   );
 };
